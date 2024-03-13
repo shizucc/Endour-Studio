641cc70c0348447da243eeb6563a22335930406f
shizucc <syifanaalfi@gmail.com>
2024-03-13 16:32:32 +0700

    navbar on progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
+import classes from "./header.module.css";
+import endourStudioImg from "../assets/endour_studio_favicon.png";
+
 export default function Header() {
   return (
-    <>
-      <p>Ini adalah Header</p>
-    </>
+    <nav className={classes.navigationBar}>
+      <section style={{ display: "flex", alignItems: "center" }}>
+        <img src={endourStudioImg} alt="" />
+        <div className={classes.navigationTitle}>
+          <h1 style={{ fontWeight: 500 }}>ENDOUR</h1>
+          <h1 style={{ fontWeight: 300 }}>STUDIO</h1>
+        </div>
+      </section>
+      <ul className={classes.navigationMenu}>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Games</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- src/pages/Home/components/about/About.jsx
+++ src/pages/Home/components/about/About.jsx
@@ -1,8 +1,4 @@
 import classes from "./about.module.css";
 export default function About() {
-  return (
-    <section className={classes.container}>
-      <h1 className={classes.title}>Ini adalah About</h1>
-    </section>
-  );
+  return <section className={classes.container}></section>;
 }
