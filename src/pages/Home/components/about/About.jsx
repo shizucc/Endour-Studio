513ff0055b3dfe7f705bad5679b3f41a1639e7db
shizucc <syifanaalfi@gmail.com>
2024-03-14 13:58:51 +0700

    about almost done

--- src/pages/Home/components/about/About.jsx
+++ src/pages/Home/components/about/About.jsx
@@ -1,4 +1,39 @@
 import classes from "./about.module.css";
 export default function About({ ...props }) {
-  return <section {...props} className={classes.container}></section>;
+  return (
+    <section {...props} className={classes.container}>
+      <div className={classes.socialMedia}>
+        <h1>Find out more about us!</h1>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/endour.std/"
+              style={{ color: "#ED8975" }}
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://twitter.com/endour_studio"
+              style={{ color: "#0084B4" }}
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://endourstudio.itch.io/"
+              style={{ color: "#292929" }}
+            >
+              itch.io
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
