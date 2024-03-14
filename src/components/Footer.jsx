bef5d6380f2f6c898d0f625d2a34694d1c0d586a
shizucc <syifanaalfi@gmail.com>
2024-03-14 13:36:40 +0700

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  bottom: 0;
+  padding: 10px 0;
+  color: white;
+  background-color: #5c8374;
+`;
+
 export default function Footer() {
   return (
-    <>
-      <p>Ini adalah Footer</p>
-    </>
+    <StyledFooter>
+      <p>
+        <b>Copyright © 2021 - 2024 Endour Studio</b>
+      </p>
+      <p>All Rights Reserved</p>
+    </StyledFooter>
   );
 }
