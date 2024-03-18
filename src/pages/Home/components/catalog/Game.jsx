37041b6b92c014987c02b81e3fde95f218361fae
Alvin Aryanta S <a.aryanta.s@gmail.com>
2024-03-19 00:45:52 +0700

    Add games catalog (kurang sidebar)

--- src/pages/Home/components/catalog/Game.jsx
+++ src/pages/Home/components/catalog/Game.jsx
@@ -1,7 +1,29 @@
-export default function Game({ title }) {
+import styles from './game.module.css';
+
+function GameDetails({ title, cover, url, desc }) {
   return (
     <>
-      <li>{title}</li>
+      <div className={styles.gameDetails}>
+        <a href={url}><img className={styles.gameCover} src={cover} alt="Cover Image"/></a>
+        <div className={styles.gameText}>
+          <a href={url}><h1>{title}</h1></a>
+          <p>{desc}</p>
+        </div>
+      </div>
     </>
   );
+}
+
+export default function Game({ title, cover, desc, url, isLast }) {
+  return isLast ?
+    <div className={styles.gameBox}>
+      <GameDetails title={title} cover={cover} desc={desc} url={url} />
+    </div>
+  :
+    <>
+      <div className={styles.gameBox}>
+        <GameDetails title={title} cover={cover} desc={desc} url={url} />
+        <hr />
+      </div>
+    </>
 }
