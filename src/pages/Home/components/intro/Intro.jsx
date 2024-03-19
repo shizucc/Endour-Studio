e534798b55b0cf04c87faf9bafe81043cc8a2ec8
shizucc <syifanaalfi@gmail.com>
2024-03-20 00:22:08 +0700

    merged branches

--- src/pages/Home/components/intro/Intro.jsx
+++ src/pages/Home/components/intro/Intro.jsx
@@ -3,14 +3,14 @@ export default function Intro({ ...props }) {
   return (
     <>
       <section className={classes.container}>
-        <div className="coloumn">
+        <div className={classes.coloumn}>
           <h1 className={classes.title}>What is Endour Studio?</h1>
           <p className={classes.desc}>
-          "Endour Studio is an amateur game studio since 2021"
+            "Endour Studio is an amateur game studio since 2021"
           </p>
           <button className={classes.button}>Let me know!</button>
         </div>
-        <div className="coloumn">
+        <div className={classes.coloumn}>
           <img src="src/assets/Logo.png" alt="" />
         </div>
       </section>
