f0b11d41d34d02bb54d743f4f1382cb874a72f66
shizucc <syifanaalfi@gmail.com>
2024-03-21 12:28:43 +0700

    refactoring intro

--- src/pages/Home/components/catalog/Catalog.jsx
+++ src/pages/Home/components/catalog/Catalog.jsx
@@ -5,11 +5,17 @@ import { games } from "./list_games";
 
 export default function Catalog({ ...props }) {
   return (
-    <section className={classes.container}>
+    <section className={classes.container} {...props}>
       <ul>
         {games.map((game, index) => (
           <li key={index}>
-            <Game title={game.title} cover={game.cover} desc={game.description} url={game.url} isLast={index+1 == games.length} />
+            <Game
+              title={game.title}
+              cover={game.cover}
+              desc={game.description}
+              url={game.url}
+              isLast={index + 1 == games.length}
+            />
           </li>
         ))}
       </ul>
--- src/pages/Home/components/intro/Intro.jsx
+++ src/pages/Home/components/intro/Intro.jsx
@@ -1,18 +1,23 @@
 import classes from "./intro.module.css";
+import endourStudioLogoImg from "../../assets/logo_for_glow.png";
+
 export default function Intro({ ...props }) {
   return (
     <>
-      <section className={classes.container}>
-        <div className={classes.coloumn}>
+      <section className={classes.container} {...props}>
+        <div className={classes.mainContent}>
           <h1 className={classes.title}>What is Endour Studio?</h1>
           <p className={classes.desc}>
-            "Endour Studio is an amateur game studio since 2021"
+            &quot;Endour Studio is an amateur game studio since 2021&quot;
           </p>
-          <button className={classes.button}>Let me know!</button>
+          <a href="#games" className={classes.button}>
+            Let me know!
+          </a>
         </div>
-        <div className={classes.coloumn}>
-          <img src="src/assets/Logo.png" alt="" />
+        <div className={classes.imageContent}>
+          <img src={endourStudioLogoImg} alt="studio-logo" />
         </div>
+        <div></div>
       </section>
     </>
   );
