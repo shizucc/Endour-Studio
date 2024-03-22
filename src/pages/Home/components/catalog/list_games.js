d04df9bf6e1b957e8dad5a8668a7aac9a5235a38
shizucc <syifanaalfi@gmail.com>
2024-03-22 11:29:10 +0700

    fix minor bug in list_games.js

--- src/pages/Home/components/catalog/list_games.js
+++ src/pages/Home/components/catalog/list_games.js
@@ -1,27 +1,26 @@
-import lykaiaImg from "../../assets/gamecover_lykaia.png";
-import osaretaImg from "../../assets/gamecover_osareta.png";
-import twaImg from "../../assets/gamecover_twa.png";
-
+import lykaiaImg from "../../assets/gamecover_lykaia.jpg";
+import osaretaImg from "../../assets/gamecover_osareta.jpg";
+import twaImg from "../../assets/gamecover_twa.jpg";
 export const games = [
   {
     title: "Lykaia",
-    cover: "src/pages/Home/assets/gamecover_lykaia.png",
+    cover: lykaiaImg,
     description:
       "Lykaia is an action game with a tower-defend gameplay concept where you play as a knight and a cowboy to protect a village by fighting hordes of werewolves.",
-    url: lykaiaImg,
+    url: "https://endourstudio.itch.io/lykaia",
   },
   {
     title: "Osareta",
-    cover: "src/pages/Home/assets/gamecover_osareta.png",
+    cover: osaretaImg,
     description:
       "Will you succeed in winning the heart of your childhood friend? Will you turn over a new leaf with another girl? The decision is yours!",
-    url: osaretaImg,
+    url: "https://endourstudio.itch.io/osananajimi-ga-wasureta",
   },
   {
     title: "Traffic Witch Academy",
-    cover: "src/pages/Home/assets/gamecover_twa.png",
+    cover: twaImg,
     description:
       "Traffic Witch Academy is an endless game about traffic education, wrapped in magic and a fantasy world.",
-    url: twaImg,
+    url: "https://endourstudio.itch.io/traffic-witch-academy",
   },
 ];
